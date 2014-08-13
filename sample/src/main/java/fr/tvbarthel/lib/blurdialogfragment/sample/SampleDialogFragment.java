6b7e9e3ed08cfd2206e79b91e18bd09244e44469
Thomas Barthélémy <thomas.barthelemy.utc@gmail.com>
2014-08-13 22:03:31 +0200

    Tweak sample

--- sample/src/main/java/fr/tvbarthel/lib/blurdialogfragment/sample/SampleDialogFragment.java
+++ sample/src/main/java/fr/tvbarthel/lib/blurdialogfragment/sample/SampleDialogFragment.java
@@ -3,7 +3,10 @@ package fr.tvbarthel.lib.blurdialogfragment.sample;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
+import android.text.util.Linkify;
 import android.view.View;
+import android.widget.TextView;
 
 import fr.tvbarthel.lib.blurdialogfragment.BlurDialogFragment;
 
@@ -24,6 +27,9 @@ public class SampleDialogFragment extends BlurDialogFragment {
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         View customView = getActivity().getLayoutInflater().inflate(R.layout.dialog_fragment, null);
+        TextView label = ((TextView) customView.findViewById(R.id.textView));
+        label.setMovementMethod(LinkMovementMethod.getInstance());
+        Linkify.addLinks(label, Linkify.WEB_URLS);
         builder.setView(customView);
         return builder.create();
     }
